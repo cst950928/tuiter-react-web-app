59034fb3515654d02ae02671f35f0453688f89b8
cst950928 <cst950928@hotmail.com>
2023-03-27 11:39:09 -0700

    a8 change API link

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import TuitsList from "../tuits/TuitsList";
 import WhatsHappening from "./whats-happening"
-import {configureStore} from "@reduxjs/toolkit";
-import tuitsReducer from "../tuits/tuits-reducer";
-import {Provider} from "react-redux";
 
-const store = configureStore({
-    reducer: {tuitsData: tuitsReducer}
-});
 const HomeComponent2 = () => {
     return (
-        <Provider store={store}>
+        <>
             <h4>Home</h4>
             <WhatsHappening/>
             <TuitsList/>
-        </Provider>
+        </>
 
     );
 };
